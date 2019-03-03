7363a82058a6e383735449cadf5c2d58eff972b0
Tatyana <Tanyusha_88@list.ru>
2019-03-03 12:44:14 +0700

    Task 7: сценарий, проходящий по всем разделам админки

--- java-example/src/test/java/ru/stqa/training/selenium/MenuClick.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MenuClick.java
@@ -1,31 +1,15 @@
 package ru.stqa.training.selenium;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.TagName;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
-import org.xml.sax.Locator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import static org.junit.Assert.assertNotNull;
 
 public class MenuClick extends TestBase {
-
-    TestBase testBase;
-
-
-//    @Test
-//    public void litecartLogin() {
-//        driver.get("http://localhost/litecart/admin/");
-//        driver.findElement(By.name("username")).sendKeys("admin");
-//        driver.findElement(By.name("password")).sendKeys("admin");
-//        driver.findElement(By.name("login")).click();
-//        wait.until(titleIs("My Store"));
-//    }
 
     @Test
     public void menuClick() {
@@ -34,37 +18,19 @@ public class MenuClick extends TestBase {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        wait.until(titleIs("My Store"));
 
-//        List<WebElement> items = driver.findElements(By.cssSelector("#app- a"));
-//
-//        int itemsCount = items.size();
-//        System.out.println(itemsCount);
-//        System.out.println(items);
-//        System.out.println(items.get(1));
-//            for (int i = 0; i <= itemsCount-1; i++){
-//
-////                List<WebElement> items = driver.findElements(By.cssSelector("#app- > a"));
-////                int itemsCount = items.size();
-//                WebElement singleItem = items.get(i);
-//                System.out.println(singleItem);
-//                singleItem.click();
-//////                testBase = testBase.isElementPresent(By.tagName("h1"));
-//
-//            }
-//        foreach (var item )
-
-        int i = 0;
-            while (true) {
-                List<WebElement> items = driver.findElements(By.cssSelector("#app- a"));
-                if (i >= items.size()) {
-                    break;
-                }
-                items.get(i++).click();
+        List<WebElement> menu = driver.findElements(By.cssSelector("li#app-"));
+        for (int i = 0; i < menu.size(); i++) {
+            menu.get(i).click();
+            assertNotNull(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h1")));
+            menu = driver.findElements(By.cssSelector("li#app-"));
+            List<WebElement> items = menu.get(i).findElements(By.cssSelector("li"));
+            for (int k = 0; k < items.size(); k++) {
+                items.get(k).click();
+                assertNotNull(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h1")));
+                menu = driver.findElements(By.cssSelector("li#app-"));
+                items = menu.get(i).findElements(By.cssSelector("li"));
             }
-
-
-
+        }
     }
-
 }
